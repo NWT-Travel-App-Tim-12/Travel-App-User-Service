af7c8c8cad0fae3bfc6dab24c25a114012ec9403
Ahmed <amahovac1@etf.unsa.ba>
2023-03-31 03:21:30 +0200

    implemented error handling

---
+++ src/main/java/com/app/travel/service/user/exceptions/ExceptionsHandler.java
@@ -0,0 +1,26 @@
+package com.app.travel.service.user.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class ExceptionsHandler {
+
+    @ExceptionHandler(value = {MethodArgumentNotValidException.class})
+    public ResponseEntity handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        Map<String, String> errors = new HashMap<>();
+        e.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return new ResponseEntity(errors, HttpStatus.BAD_REQUEST);
+    }
+}
