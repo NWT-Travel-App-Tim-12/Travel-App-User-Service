988e8693f56a70fdb8e2d69729f0c71ceb6c90ef
Ahmed <amahovac1@etf.unsa.ba>
2023-04-07 09:49:30 +0200

    added centralized config server config

--- src/main/java/com/app/travel/service/user/Application.java
+++ src/main/java/com/app/travel/service/user/Application.java
@@ -6,9 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+
+
+@EnableDiscoveryClient
 @SpringBootApplication
 @OpenAPIDefinition
-@EnableDiscoveryClient
 public class Application {
  // comm
 	public static void main(String[] args) {
